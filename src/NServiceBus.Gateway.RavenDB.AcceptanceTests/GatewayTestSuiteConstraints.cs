8245d9463e5f4fdb5e7de453b17b6a712bdb54ef
mauroservienti <mauro.servienti@gmail.com>
2020-01-30 15:20:31 +0100

    Empty solution structure

---
+++ src/NServiceBus.Gateway.RavenDB.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -0,0 +1,18 @@
+﻿using NServiceBus.AcceptanceTesting.Support;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Gateway.AcceptanceTests
+{
+    public partial class GatewayTestSuiteConstraints : IGatewayTestSuiteConstraints
+    {
+        public Task ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task Cleanup()
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
