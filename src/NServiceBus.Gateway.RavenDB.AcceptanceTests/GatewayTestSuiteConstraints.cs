9df21206325128d9722971e9cb52f0c15dd4e143
mauroservienti <mauro.servienti@gmail.com>
2020-01-31 14:49:28 +0100

    Test suite constraints

--- src/NServiceBus.Gateway.RavenDB.AcceptanceTests/GatewayTestSuiteConstraints.cs
+++ src/NServiceBus.Gateway.RavenDB.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -1,6 +1,10 @@
 ﻿using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.Configuration.AdvancedExtensibility;
 using NServiceBus.Gateway.RavenDB;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using System;
 using System.Threading.Tasks;
 
 namespace NServiceBus.Gateway.AcceptanceTests
@@ -11,19 +15,70 @@ namespace NServiceBus.Gateway.AcceptanceTests
         {
             var ravenGatewayDeduplicationConfiguration = new RavenGatewayDeduplicationConfiguration((builder, _)=> 
             {
-                return null;
+                databaseName = Guid.NewGuid().ToString();
+                var documentStore = GetInitializedDocumentStore(databaseName);
+
+                documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(databaseName)));
+
+                return documentStore;
             });
+
             var gatewaySettings = configuration.Gateway(ravenGatewayDeduplicationConfiguration);
             configuration.GetSettings().Set(gatewaySettings);
 
             return Task.FromResult(false);
         }
 
-        public Task Cleanup()
+        public async Task Cleanup()
         {
-            //TODO: clean up db once tests are completed
+            // Periodically the delete will throw an exception because Raven has the database locked
+            // To solve this we have a retry loop with a delay
+            var triesLeft = 3;
 
-            return Task.FromResult(false);
+            while (triesLeft-- > 0)
+            {
+                try
+                {
+                    // We are using a new store because the global one is disposed of before cleanup
+                    using (var storeForDeletion = GetInitializedDocumentStore(databaseName))
+                    {
+                        storeForDeletion.Maintenance.Server.Send(new DeleteDatabasesOperation(storeForDeletion.Database, hardDelete: true));
+                        break;
+                    }
+                }
+                catch
+                {
+                    if (triesLeft == 0)
+                    {
+                        throw;
+                    }
+
+                    await Task.Delay(250);
+                }
+            }
+
+            Console.WriteLine("Deleted '{0}' database", databaseName);
         }
+
+        static DocumentStore GetInitializedDocumentStore(string defaultDatabase)
+        {
+            var urls = Environment.GetEnvironmentVariable("CommaSeparatedRavenClusterUrls");
+            if (urls == null)
+            {
+                throw new Exception("RavenDB cluster URLs must be specified in an environment variable named CommaSeparatedRavenClusterUrls.");
+            }
+
+            var documentStore = new DocumentStore
+            {
+                Urls = urls.Split(','),
+                Database = defaultDatabase
+            };
+
+            documentStore.Initialize();
+
+            return documentStore;
+        }
+
+        string databaseName;
     }
 }
