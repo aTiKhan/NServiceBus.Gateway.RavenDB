099eb40c2174795eb7cbe409f1782068042ff119
mauroservienti <mauro.servienti@gmail.com>
2020-02-03 10:50:15 +0100

    Disable "AutoPropertyCanBeMadeGetOnly" code inspection once

--- src/NServiceBus.Gateway.RavenDB/RavenGatewayDeduplicationConfiguration.cs
+++ src/NServiceBus.Gateway.RavenDB/RavenGatewayDeduplicationConfiguration.cs
@@ -78,6 +78,7 @@ namespace NServiceBus.Gateway.RavenDB
         /// <summary>
         /// The time to keep deduplication information, default value is 7 days
         /// </summary>
+        //ReSharper disable once AutoPropertyCanBeMadeGetOnly
         public TimeSpan DeduplicationDataTimeToLive { get; set; } = TimeSpan.FromDays(7);
 
         ReadOnlySettings settings;
